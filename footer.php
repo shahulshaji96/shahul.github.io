b089585ef3cc2de7a755431ff860124b0920ca63
shahulshaji96 <shahul.mltl@gmail.com>
2020-08-24 20:17:00 +0530

    responsive

--- footer.php
+++ footer.php
@@ -3,25 +3,25 @@
 <!-- start #footer -->
 <footer  id="footer" class="bg-dark text-white ">
      <div class="container mb-2 " >
-        <div class="row mx-3 mt-2 py-3 ">
-            <div class="col-lg-3 col-auto">
+        <div class="row mx-2 mt-2 pt-3 ">
+            <div class="col-lg-3 col-sm-12 col-auto mx-1">
                 <h4 class="font-rubik font-size-20">Contact Me</h4>
                 <p class="font-size-14 font-rale text-white-50">I would appreciate if you could pursue my application
 					 towards the suitable position in your highly esteemed organization. </p>
             </div>
-            <div class="col-lg-4 col-sm-auto  col-12 mb-2 mx-1">
+            <div class="col-lg-4 col-md-auto  col-sm-12  col-12 mb-2">
                 <h4 class="font-rubik font-size-20">Email</h4>
                 <form class="form-row ">
-                    <div class="col">
+                    <div class="col-7">
                         <input type="text" class="form-control" placeholder="Email *">
                     </div>
-                    <div class="col">
+                    <div class="col-3">
                         <button type="submit" class="btn btn-primary mb-2">Submit</button>
                     </div>
                 </form>
             </div>
-            <div class="col-lg-2 col-sm-auto col-6 mx-1">
-                <h4 class="font-rubik font-size-20">Address</h4>
+            <div class="col-lg-2 col-md-auto col-sm-4 col-6 ">
+                <h4 class="font-rubik font-size-20">My Address</h4>
                 <div class="d-flex flex-column flex-wrap">
                     <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Location</a>
                     <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Ernakulam Dist</a>
@@ -29,7 +29,7 @@
                     <a href="#" class="font-rale font-size-14 text-white-50 pb-1">India</a>
                 </div>
             </div>
-            <div class="col-lg-2 col-sm-auto col-6 mx-1">
+            <div class="col-lg-2  col-sm-4 col-auto">
                 <h4 class="font-rubik font-size-20">My hobbies</h4>
                 <div class="d-flex flex-column flex-wrap">
                     <a href="#" class="font-rale font-size-14 text-white-50 pb-1">Cycling</a>
